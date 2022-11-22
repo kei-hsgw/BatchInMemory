ba3a5fb1e3f527402fb6b4161ec880aeec0d0b39
keisukehasegawa0918 <chiro9689@gmail.com>
2022-11-22 16:55:53 +0900

    ドメインクラス作成

--- src/main/java/com/example/demo/domain/Employee.java
+++ src/main/java/com/example/demo/domain/Employee.java
@@ -1,5 +1,20 @@
 package com.example.demo.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 
+	/** ID */
+	private Integer id;
+	/** 名前 */
+	private String name;
+	/** 年齢 */
+	private Integer age;
+	/** 性別 */
+	private Integer gender;
 }
